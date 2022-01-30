4ea191349eb29ae5b3960b019b2fb7601be5d843
yavor <yavormarinov7@gmail.com>
2022-01-30 18:01:57 +0200

    Implement Pet Clinic POJO Data Model

---
+++ src/main/java/ym/group/sfgpetclinic/model/PetType.java
@@ -0,0 +1,13 @@
+package ym.group.sfgpetclinic.model;
+
+public class PetType {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
